70b927a0b529747f6fa7c857d4411a81af723c35
Juan Pablo Romero <jryn145@gmail.com>
2022-10-13 16:01:55 -0400

    set status 403 forbiden en caso de password o username invalido

--- API/Routes/Controllers/login.js
+++ API/Routes/Controllers/login.js
@@ -3,18 +3,20 @@ const bcrypt = require('bcryptjs');
 
 const signIn = async (req,res) => {
     const {username, password } = req.body;
-    let hashedPassword = await bcrypt.hash(password, 8);
+
     const user = await profile.findOne({
         where:{ name:username }
     });
 
     const passwordCorrect = user === null ? false : await bcrypt.compare(password,user.password);
-    
+
     try{
         if(passwordCorrect){
-            res.send("Welcome")
+            res.send("Welcome");
         }else{
-            res.send("user or password incorrect");
+            res.status(403).json({
+                error:"invalid user or password",
+            })
         }
     }catch(err){
         res.send(err.message);
