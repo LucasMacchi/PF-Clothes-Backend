1e15d6bc15cf779a4fe4a41a531982926cd2e793
Micael Picco <micaelpicco@MacBook-Pro-de-Micael.local>
2022-10-13 10:02:32 -0300

    ‘Ruta_para_modificar_usuario’

--- API/Routes/userRoutes.js
+++ API/Routes/userRoutes.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const { Profile } = require("../DataBase/db");
 const router = Router();
 
+//Crear un usuario
 router.post("/", async (req, res) => {
   const {
     name,
@@ -32,6 +33,22 @@ router.post("/", async (req, res) => {
       },
     });
     return res.send(user);
+  } catch (err) {
+    res.send(err.message);
+  }
+});
+
+//Modificar datos de un usuario
+router.put("/:id", async (req, res) => {
+  const { id } = req.params;
+  const object = req.body;
+  try {
+    let modifyUser = await Profile.update(object, {
+      where: {
+        id: id,
+      },
+    });
+    return res.send({ changed: true });
   } catch (err) {
     res.send(err.message);
   }
