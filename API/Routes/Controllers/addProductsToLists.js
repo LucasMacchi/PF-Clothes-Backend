59ccac4348b74e047a1c0d0948eae94d672cb7a9
Lucas Macchi <lucasmacchi25@gmail.com>
2022-10-13 16:04:45 -0300

    nombre de parametros cambiado para mayor claridad

--- API/Routes/Controllers/addProductsToLists.js
+++ API/Routes/Controllers/addProductsToLists.js
@@ -1,10 +1,10 @@
 const {profile,Op} = require('../../DataBase/db');
 
 
-const addProductsToLists = async(prodID, proid, order) => {
+const addProductsToLists = async(productID, proid, order) => {
     if(order === "fav"){
         try {
-            await profile.update({favorites: favorites.concat(prodID)}, {where:{id:proid}})
+            await profile.update({favorites: favorites.concat(productID)}, {where:{id:proid}})
             return "El producto se agrego a favoritos"
         } catch (error) {
             throw Error(error)
@@ -12,7 +12,7 @@ const addProductsToLists = async(prodID, proid, order) => {
     }
     else if(order === "shop"){
         try {
-            await profile.update({shoppingCart: shoppingCart.concat(prodID)}, {where:{id:proid}})
+            await profile.update({shoppingCart: shoppingCart.concat(productID)}, {where:{id:proid}})
             return "El producto se agrego al carrito"
         } catch (error) {
             throw Error(error)
