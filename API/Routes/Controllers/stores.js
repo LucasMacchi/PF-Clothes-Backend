b52083d1c80fb3fadd1b820f12602e3e6a3242c9
Juan Pablo Romero <jryn145@gmail.com>
2022-10-12 22:47:12 -0400

    ruta filtrar tiendas por nombre

--- API/Routes/Controllers/stores.js
+++ API/Routes/Controllers/stores.js
@@ -17,6 +17,24 @@ const getAllStores = async (req,res) => {
     }
 }
 
+const getStoreByName = async (req,res) => {
+    
+    let {name} = req.params;
+
+    const store = await profile.findAll({
+        where:{
+            storeName:name,
+        }
+    });
+
+    try{
+        res.send(store);
+    }catch(err){
+        res.send(err.message);
+    }
+}
+
 module.exports = {
     getAllStores,
+    getStoreByName,
 };
--- API/Routes/storeRoutes.js
+++ API/Routes/storeRoutes.js
@@ -2,9 +2,11 @@ const { Router } = require("express");
 const router = Router();
 const {
     getAllStores,
-} = require('./Controllers/getAllStores');
+    getStoreByName,
+} = require('./Controllers/stores');
 
 // rutas vendedores
 router.get("/all",getAllStores);
+router.get("/:name",getStoreByName);
 
 module.exports = router;
