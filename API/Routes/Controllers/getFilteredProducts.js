67fb41e6ba20a79d215d660ef8a103394ba9acfc
Micael Picco <micaelpicco@MacBook-Pro-de-Micael.local>
2022-10-14 10:14:18 -0300

    ‘Ruta_para_todos_los_filtrados_de_los_productos’

---
+++ API/Routes/Controllers/getFilteredProducts.js
@@ -0,0 +1,83 @@
+const { product } = require("../../DataBase/db");
+const paginated = require("./paginado");
+
+const getFilteredProducts = async (
+  size,
+  price,
+  demographic,
+  cant,
+  sortBy,
+  orderBy
+) => {
+  if (size && price && demographic) {
+    let filteredProducts = await product.findAll({
+      where: {
+        size,
+        price,
+        demographic,
+      },
+      order: [[sortBy, orderBy]],
+    });
+    let data = await filteredProducts;
+    if (!data.length) throw Error("No existe ningun producto");
+    if (cant) {
+      return paginated(data, cant);
+    } else return data;
+  } else if (size && price) {
+    let filteredProducts = await product.findAll({
+      where: {
+        size,
+        price,
+      },
+      order: [[sortBy, orderBy]],
+    });
+    let data = await filteredProducts;
+    if (!data.length) throw Error("No existe ningun producto");
+    if (cant) {
+      return paginated(data, cant);
+    } else return data;
+  } else if (size && demographic) {
+    let filteredProducts = await product.findAll({
+      where: {
+        size,
+        demographic,
+      },
+      order: [[sortBy, orderBy]],
+    });
+    let data = await filteredProducts;
+    if (!data.length) throw Error("No existe ningun producto");
+    if (cant) {
+      return paginated(data, cant);
+    } else return data;
+  } else if (price && demographic) {
+    let filteredProducts = await product.findAll({
+      where: {
+        price,
+        demographic,
+      },
+      order: [[sortBy, orderBy]],
+    });
+    let data = await filteredProducts;
+    if (!data.length) throw Error("No existe ningun producto");
+    if (cant) {
+      return paginated(data, cant);
+    } else return data;
+  }
+  if (size || price || demographic) {
+    let filteredProducts = await product.findAll({
+      where: {
+        size,
+        price,
+        demographic,
+      },
+      order: [[sortBy, orderBy]],
+    });
+    let data = await filteredProducts;
+    if (!data.length) throw Error("No existe ningun producto");
+    if (cant) {
+      return paginated(data, cant);
+    } else return data;
+  }
+};
+
+module.exports = getFilteredProducts;
--- API/Routes/productRoutes.js
+++ API/Routes/productRoutes.js
@@ -1,50 +1,68 @@
 const { Router } = require("express");
 
 //Aca van los archivos de los controladores
-const getAllProducts = require("./Controllers/getAllProducts")
-const getProductName = require("./Controllers/getAllProductsByName")
+const getAllProducts = require("./Controllers/getAllProducts");
+const getProductName = require("./Controllers/getAllProductsByName");
+const getFilteredProducts = require("./Controllers/getFilteredProducts");
+
 //
 const router = Router();
 
 //Aca abajo van las rutas separadas
 //Trae todos los productos con una cantidad especificada por arreglo
 router.get("/all/:cant", async (req, res) => {
-    const cant = req.params.cant
-    try {
-        const allproducts = await getAllProducts(cant)
-        res.status(200).send(allproducts)
-    } catch (error) {
-        res.status(404).send(error.message)
-    }
-})
+  const cant = req.params.cant;
+  try {
+    const allproducts = await getAllProducts(cant);
+    res.status(200).send(allproducts);
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
 //Trae todos los productos
 router.get("/all", async (req, res) => {
-    try {
-        const allproducts = await getAllProducts(0)
-        res.status(200).send(allproducts)
-    } catch (error) {
-        res.status(404).send(error.message)
-    }
-})
+  try {
+    const allproducts = await getAllProducts(0);
+    res.status(200).send(allproducts);
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
 //
 //Traer todos los productos por nombre / Trae por query la variable search (string de busqueda) y cant (cantidad de elementos en paginado)
 router.get("/", async (req, res) => {
-    const search = req.query.search
-    const cant = req.query.cant
-    try {
-        if(cant){
-            const productsByName = await getProductName(search, cant)
-            res.status(200).send(productsByName)
-        }
-        else{
-            const productsByName = await getProductName(search, 0)
-            res.status(200).send(productsByName)
-        }
-    } catch (error) {
-        res.status(404).send(error)
+  const search = req.query.search;
+  const cant = req.query.cant;
+  try {
+    if (cant) {
+      const productsByName = await getProductName(search, cant);
+      res.status(200).send(productsByName);
+    } else {
+      const productsByName = await getProductName(search, 0);
+      res.status(200).send(productsByName);
     }
-    
-})
+  } catch (error) {
+    res.status(404).send(error);
+  }
+});
 //
+//Filtrados
+router.get("/", async (req, res) => {
+  const { size, price, demographic, cant = 0, sortBy, orderBy } = req.query;
+  try {
+    const products = await getFilteredProducts(
+      size,
+      price,
+      demographic,
+      cant,
+      sortBy,
+      orderBy
+    );
+    res.status(200).send(products);
+  } catch (error) {
+    res.status(404).send(error.message);
+  }
+});
+
 //exportamos el router
 module.exports = router;
