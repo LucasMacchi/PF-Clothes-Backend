91d8abe7ae53d846f5e8dde27d8bfb20780ad4f8
Juan Pablo Romero <jryn145@gmail.com>
2022-10-15 01:19:07 -0400

    agregando formato a logger

--- API/Routes/Utils/logger.js
+++ API/Routes/Utils/logger.js
@@ -1,6 +1,9 @@
 const logger = (request,response,next) => {
-    console.log(request.method);
-    console.log(request.path);
+    console.log("==========");
+    console.log("request");
+    console.log(`method: ${request.method}`);
+    console.log(`path: ${request.path}`);
+    console.log('body: ');
     console.log(request.body);
     console.log("==========");
     next();
