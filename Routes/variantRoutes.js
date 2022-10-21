5c032511373912e76c20e3024aec6d6ef049c8b8
Lucas Macchi <lucasmacchi25@gmail.com>
2022-10-21 20:02:07 -0300

    ruta para eliminar y modificar variantes

--- Routes/variantRoutes.js
+++ Routes/variantRoutes.js
@@ -2,11 +2,36 @@ const { Router } = require("express");
 const router = Router();
 //Controllers
 const getVariant = require("./Controllers/getVariant")
+const putVariant = require("./Controllers/putVariant")
+const deleteVariant = require("./Controllers/deleteVariant")
 
 router.get("/:id", async (req, res) => {
     const id = req.params.id
     try {
       const response = await getVariant(id)
+      
+      res.status(200).send(response);
+    } catch (error) {
+      res.status(404).send(error.message);
+    }
+  });
+
+  router.put("/:id", async (req, res) => {
+    const id = req.params.id
+    
+    try {
+      const response = await putVariant(id,req.body)
+      console.log("response = ",response)
+      res.status(200).send(response);
+    } catch (error) {
+      res.status(404).send(error.message);
+    }
+  });
+
+  router.delete("/:id", async (req, res) => {
+    const id = req.params.id
+    try {
+      const response = await deleteVariant(id)
       res.status(200).send(response);
     } catch (error) {
       res.status(404).send(error.message);
