95601c48a50a5fa2496201bdad8947e2398f99f7
Juan Pablo Romero <jryn145@gmail.com>
2022-10-23 15:58:01 -0400

    implement token with google login

--- Routes/Controllers/getUser.js
+++ Routes/Controllers/getUser.js
@@ -1,5 +1,25 @@
-const getUser = async (req,res) => {
-    res.json(req.user);
+const jwt = require("jsonwebtoken");
+
+const getUser = async (req,res,next) => {
+    console.log("getting user");
+
+    try{
+        if(req.user){
+            const token = jwt.sign({
+                id:req.user.id,
+                username:req.user.username,
+            },
+            process.env.SECRET,
+            {expiresIn:60*60*24});
+            res.send({
+                username:req.user.username,
+                token,
+            });
+        }
+    }catch(err){
+        next(err);
+    }
+    
 }
 
 module.exports = {
