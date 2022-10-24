295f4e8c2ef2d9d5829b8e06c2a828755a40ec45
Lucas Macchi <lucasmacchi25@gmail.com>
2022-10-23 21:57:25 -0300

    force

--- index.js
+++ index.js
@@ -5,10 +5,10 @@ const {conn, profilesCreator} = require("./DataBase/db")
 const db = conn
 
 
-db.sync({force: false}).then(()=> {
+db.sync({force: true}).then(()=> {
   server.listen(process.env.PORT, async() => {
     try {
-      //await profilesCreator()
+      await profilesCreator()
       db.authenticate().then(()=> console.log("database connected"))
       console.log("Server is up, at port ", process.env.PORT);
     } catch (error) {
