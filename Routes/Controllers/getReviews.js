defe5a5bdbb4df1d020c0379de764f34abddca91
Micael Picco <micaelpicco@MacBook-Pro-de-Micael.local>
2022-10-25 03:47:54 -0300

    Eliminacion_del_throw_error_por_traer_problemas

--- Routes/Controllers/getReviews.js
+++ Routes/Controllers/getReviews.js
@@ -1,19 +1,15 @@
-const {qualification} = require('../../DataBase/db');
+const { qualification } = require("../../DataBase/db");
 
 const getReview = async (id, order) => {
-    if(order === "product"){
-        const review = await qualification.findAll({where:{productId: id}})
-        const data = await review
-        if(!data.length) throw Error("Este producto no tiene reviews") 
-        return data
-    }
-    else if(order === "profile"){
-        const review = await qualification.findAll({where:{profileId: id}})
-        const data = await review
-        if(!data.length) throw Error("Este perfil no tiene reviews") 
-        return data
-    }
-    else throw Error("Orden incorrecta") 
-}
+  if (order === "product") {
+    const review = await qualification.findAll({ where: { productId: id } });
+    const data = await review;
+    return data;
+  } else if (order === "profile") {
+    const review = await qualification.findAll({ where: { profileId: id } });
+    const data = await review;
+    return data;
+  } else throw Error("Orden incorrecta");
+};
 
-module.exports = getReview
+module.exports = getReview;
