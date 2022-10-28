874e3f3f3428de33fa34119de154408d3999c0cd
Carsefer <107973841+Carsefer@users.noreply.github.com>
2022-10-28 11:24:36 -0500

    patch añadido al header

--- Routes/userRoutes.js
+++ Routes/userRoutes.js
@@ -2,9 +2,9 @@ const { Router } = require("express");
 var bcrypt = require("bcryptjs");
 const { profile } = require("../DataBase/db");
 const router = Router();
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 //utils
-const url = require("./Utils/imageUploader")
+const url = require("./Utils/imageUploader");
 //Controladores
 const addProductsToLists = require("./Controllers/addProductsToLists");
 const deleteProductsOfList = require("./Controllers/deleteProductsOfList");
@@ -14,8 +14,8 @@ const getAvrg = require("./Controllers/avrgScore");
 const { getToken } = require("./Utils/getToken");
 const getShoppingcart = require("./Controllers/getShoppingcart");
 const getFavoritesList = require("./Controllers/getFavoritesList");
-const patchProfile = require("./Controllers/patchProfile")
-const passport = require('passport');
+const patchProfile = require("./Controllers/patchProfile");
+const passport = require("passport");
 // crear usuario
 router.post("/", async (req, res) => {
   let {
@@ -33,14 +33,15 @@ router.post("/", async (req, res) => {
   } = req.body;
   //Image uploaders
   try {
-    profilePicture = await url(req.files["profilePicture"][0].path)
+    profilePicture = await url(req.files["profilePicture"][0].path);
   } catch (error) {
-    profilePicture = "https://res.cloudinary.com/pfclothhenry2022/image/upload/v1666385099/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture_rgmldn.webp"
+    profilePicture =
+      "https://res.cloudinary.com/pfclothhenry2022/image/upload/v1666385099/depositphotos_39258143-stock-illustration-businessman-avatar-profile-picture_rgmldn.webp";
   }
   try {
-    banner = await url(req.files["banner"][0].path)
+    banner = await url(req.files["banner"][0].path);
   } catch (error) {
-    banner = null
+    banner = null;
   }
   //
   let passwordHash = await bcrypt.hash(password, 8);
@@ -48,7 +49,7 @@ router.post("/", async (req, res) => {
   if (!favorites) favorites = [];
   if (!shoppingCart) shoppingCart = [];
 
-  try {  
+  try {
     try {
       let [user, created] = await profile.findOrCreate({
         where: {
@@ -67,97 +68,127 @@ router.post("/", async (req, res) => {
       });
       res.send(user);
     } catch (error) {
-      throw Error(error.message)
+      throw Error(error.message);
     }
-    
-    
   } catch (err) {
     res.status(404).send(err.message);
   }
 });
 //Modify profile
-router.patch("/",passport.authenticate('jwt',{session:false}), async (req, res) => {
-  try {
-    const response = await patchProfile(req);
-    res.status(200).send(response);
-  } catch (error) {
-    res.status(404).send(error.message);
+router.patch(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    try {
+      const response = await patchProfile(req);
+      res.status(200).send(response);
+    } catch (error) {
+      res.status(404).send(error.message);
+    }
   }
-});
+);
 //add elements to favorites
-router.put("/favorites", passport.authenticate('jwt',{session:false}), async (req, res) => {
-  const { productID, profileID } = req.query;
-  try {
-    const response = await addProductsToLists(productID, profileID, "fav");
-    res.status(200).send(response);
-  } catch (error) {
-    res.status(404).send(error.message);
+router.put(
+  "/favorites",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { productID, profileID } = req.query;
+    try {
+      const response = await addProductsToLists(productID, profileID, "fav");
+      res.status(200).send(response);
+    } catch (error) {
+      res.status(404).send(error.message);
+    }
   }
-});
+);
 
 //add elements to shoppingcart
-router.put("/shoppingcart", passport.authenticate('jwt',{session:false}), async (req, res) => {
-  const { productID, profileID } = req.query;
-  try {
-    const response = await addProductsToLists(productID, profileID, "shop");
-    res.status(200).send(response);
-  } catch (error) {
-    res.status(404).send(error.message);
+router.put(
+  "/shoppingcart",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { productID, profileID } = req.query;
+    try {
+      const response = await addProductsToLists(productID, profileID, "shop");
+      res.status(200).send(response);
+    } catch (error) {
+      res.status(404).send(error.message);
+    }
   }
-});
+);
 //remove elements of favorites
-router.delete("/favorites", passport.authenticate('jwt',{session:false}), async (req, res) => {
-  const { productID, profileID } = req.query;
-  try {
-    const response = await deleteProductsOfList(productID, profileID, "fav");
-    res.status(200).send(response);
-  } catch (error) {
-    res.status(404).send(error.message);
+router.delete(
+  "/favorites",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { productID, profileID } = req.query;
+    try {
+      const response = await deleteProductsOfList(productID, profileID, "fav");
+      res.status(200).send(response);
+    } catch (error) {
+      res.status(404).send(error.message);
+    }
   }
-});
+);
 //remove elements of the shopping card
-router.delete("/shoppingcart", passport.authenticate('jwt',{session:false}), async (req, res) => {
-  const { productID, profileID } = req.query;
-  try {
-    const response = await deleteProductsOfList(productID, profileID, "shop");
-    res.status(200).send(response);
-  } catch (error) {
-    res.status(404).send(error.message);
+router.delete(
+  "/shoppingcart",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { productID, profileID } = req.query;
+    try {
+      const response = await deleteProductsOfList(productID, profileID, "shop");
+      res.status(200).send(response);
+    } catch (error) {
+      res.status(404).send(error.message);
+    }
   }
-});
+);
 //get all elements of the shopping card
-router.get("/shoppingcart", passport.authenticate('jwt',{session:false}), async (req, res) => {
-  const { profileID } = req.query;
-  try {
-    const response = await getShoppingcart(profileID);
-    res.status(200).send(response);
-  } catch (error) {
-    res.status(404).send(error.message);
+router.get(
+  "/shoppingcart",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { profileID } = req.query;
+    try {
+      const response = await getShoppingcart(profileID);
+      res.status(200).send(response);
+    } catch (error) {
+      res.status(404).send(error.message);
+    }
   }
-});
+);
 
 //get all elements of the favorites list
-router.get("/favorites", passport.authenticate('jwt',{session:false}), async (req, res) => {
-  const { profileID } = req.query;
-  try {
-    const response = await getFavoritesList(profileID);
-    res.status(200).send(response);
-  } catch (error) {
-    res.status(404).send(error.message);
+router.get(
+  "/favorites",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { profileID } = req.query;
+    try {
+      const response = await getFavoritesList(profileID);
+      res.status(200).send(response);
+    } catch (error) {
+      res.status(404).send(error.message);
+    }
   }
-});
+);
 
 //Request data from user
-router.get("/get", passport.authenticate('jwt',{session:false}), async (req, res) => {
-  const {secret_token} = req.query;
-  const decodedToken = jwt.verify(secret_token,process.env.SECRET);
-  try {
-    let user = await profile.findByPk(decodedToken.id);
-    return res.send(user);
-  } catch (err) {
-    res.send(err.message);
+router.get(
+  "/get",
+  passport.authenticate("jwt", { session: false }),
+  async (req, res) => {
+    const { secret_token } = req.query;
+    const decodedToken = jwt.verify(secret_token, process.env.SECRET);
+    try {
+      let user = await profile.findByPk(decodedToken.id);
+      return res.send(user);
+    } catch (err) {
+      res.send(err.message);
+    }
   }
-});
+);
 
 //Trae las reviews al usuario
 router.get("/review/:id", async (req, res) => {
@@ -190,6 +221,5 @@ router.post("/review/:id", getToken, async (req, res) => {
     res.status(404).send(error.message);
   }
 });
-
 
 module.exports = router;
