a0c48470dcf0398f9741318d5f3751d789318c45
LucasMacchi <lucasmacchi25@gmail.com>
2022-11-01 17:04:59 -0300

    fix

--- Routes/Controllers/getOnSell.js
+++ Routes/Controllers/getOnSell.js
@@ -1,7 +1,7 @@
 const {product, profile} = require('../../DataBase/db');
 
 const onSell = async (id) => {
-    const onSellProducts = await product.findAll({raw:true},{where:{profileId:id}})
+    const onSellProducts = await product.findAll({where:{profileId:id}})
     return await onSellProducts
 }
 
