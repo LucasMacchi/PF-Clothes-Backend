c696d575cbec1498467d1d826aa6d0789b6b220b
Carsefer <107973841+Carsefer@users.noreply.github.com>
2022-11-01 14:29:50 -0500

    a

--- DataBase/Models/Variant.js
+++ DataBase/Models/Variant.js
@@ -2,27 +2,27 @@ const { DataTypes } = require("sequelize");
 const { sizes } = require("../utils/sizes.js");
 
 module.exports = (sequelize) => {
-    sequelize.define("variant", {
-        id: {
-            type: DataTypes.UUID,
-            primaryKey: true,
-            allowNull: false,
-            defaultValue: DataTypes.UUIDV4,
-          },
-          size: {
-            type: DataTypes.STRING,
-            allowNull: false,
-            validator: {
-              isIn: [sizes],
-            },
-          },
-          color: {
-            type: DataTypes.STRING,
-            allowNull: false,
-          },
-          stock: {
-            type: DataTypes.INTEGER,
-            allowNull: false,
-          },
-    })
-}
+  sequelize.define("variant", {
+    id: {
+      type: DataTypes.UUID,
+      primaryKey: true,
+      allowNull: false,
+      defaultValue: DataTypes.UUIDV4,
+    },
+    size: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validator: {
+        isIn: [sizes],
+      },
+    },
+    color: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  });
+};
